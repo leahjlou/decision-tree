4e75c0a0a705545ec1dd1278c67a774b7c18ceef
Leah Loughran <lloughran@hirevue.com>
2013-12-10 03:10:17 -0700

    Finished decision tree algorithm implementation.

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
+import math
+
 class Node:
     def __init__(self, name):
         self.name = name
         self.label = None
         self.decisionAttr = None
+        self.decisionGain = None
         self.branches = []
+
+    def printTree(self):
+        self.printTreeRecurse(0)
+
+    def printTreeRecurse(self, level):
+        print '\t' * level + self.name,
+
+        if self.decisionAttr and self.decisionGain:
+            print 'split by ' + str(self.decisionAttr) + ' for a gain of ' + str(self.decisionGain),
+
+        if self.label:
+            print ' ' + self.label,
+
+        print '\n',
+
+        level += 1
+        for branch in self.branches:
+            branch.printTreeRecurse(level)
 
 
 # Returns the root node of the constructed decision tree
 def constructDecisionTree(examples, targetAttribute, attributes):
-    root = Node('Decision Tree Root')
+    root = Node('')
 
     # Examples are all positive
-    if all(example.label == '+' for example in examples):
+    # The last attribute in the example is
+
+    if all(isPositive(example[-1]) for example in examples):
         root.label = '+'
         return root
 
     # Examples are all negative
-    elif all(example.label == '-' for example in examples):
+    elif all(not isPositive(example[-1]) for example in examples):
         root.label = '-'
         return root
 
@@ -25,22 +48,41 @@ def constructDecisionTree(examples, targetAttribute, attributes):
         root.label = getMostCommonLabel(examples)
         return root
     else:
-        attr = getHighestInfoGainAttr(attributes, examples)
+        result = getHighestInfoGainAttr(attributes, examples)
+        attr = result[0]
+        gain = result[1]
+        attrIndex = result[2]
+
         root.decisionAttr = attr
-        possibleValues = uniqueValues(attr, examples)
+        root.decisionGain = gain
+
+        possibleValues = uniqueValues(attrIndex, examples)
 
         for value in possibleValues:
             newBranch = Node(attr + ' = ' + value)
             root.branches.append(newBranch)
-            branchExamplesGen = (row for row in examples if row[attr] == value)
-            branchExamples = sorted(branchExamplesGen)
+            branchExamples = sorted(row for row in examples if row[attrIndex] == value)
 
             if not branchExamples:
                 leaf = Node(getMostCommonValue(targetAttribute, examples, possibleValues))
                 newBranch.branches.append(leaf)
             else:
-                attributes.remove(attr)
-                newBranch.branches.append(constructDecisionTree(branchExamples, targetAttribute, attributes))
+                newExamples = []
+                for example in branchExamples:
+                    newExample = []
+                    for i in range(len(example)):
+                        if not i == attrIndex:
+                            newExample.append(example[i])
+                    newExamples.append(newExample)
+
+                newBranch.branches.append(constructDecisionTree(newExamples, targetAttribute, [a for a in attributes if not a == attr]))
+
+    return root
+
+# Determines whether a word is positive ('yes', 'true', etc.)
+def isPositive(word):
+    word = word.lower()
+    return word == 'yes' or word == 'true' or word == 'y' or word == 't'
 
 
 # Returns the most common label (+ or -) in the given list of nodes
@@ -60,16 +102,92 @@ def getMostCommonLabel(nodes):
         return '-'
 
 
-# Returns the attribute from attributes with the highest information gain that best classifies examples.
-# TODO: implement
+# Returns the attribute with the highest information gain, as well as the info gain value
 def getHighestInfoGainAttr(attributes, examples):
-    return attributes[0];
+    totalRows = len(examples)
+    # Divide examples into positive and negative
+    posExamples = sorted(row for row in examples if isPositive(row[-1]))
+    negExamples = sorted(row for row in examples if not isPositive(row[-1]))
+
+    # Get the expected info needed for the entire data set
+    allExpectedInfo = computeExpectedInfo(len(posExamples), len(negExamples))
+
+    valuesGain = []
+
+    # Compute the entropy & gain of each attribute
+    for i, attr in enumerate(attributes):
+
+        # Don't check the target attribute
+        if attributes[-1] == attributes[i]:
+            break
+
+        values = uniqueValues(i, examples)
+
+        # Lists for the expected info & probability of each value
+        valuesExpectedInfo = []
+        valuesProbability = []
+
+        # Compute the expected info needed for each value
+        for value in values:
+            # Count how many positive & negative examples there are for the value
+            posExamplesOfValue = sorted(row for row in posExamples if row[i]==value)
+            negExamplesOfValue = sorted(row for row in negExamples if row[i]==value)
+            numPos = len(posExamplesOfValue)
+            numNeg = len(negExamplesOfValue)
+            # Compute the expected info & probability of the value & add them to the lists
+            valueExpectedInfo = computeExpectedInfo(numPos, numNeg)
+            valueProbability = float(numPos + numNeg) / float(totalRows)
+            valuesExpectedInfo.append(valueExpectedInfo)
+            valuesProbability.append(valueProbability)
+
+        # Compute entropy & gain of value and add gain to the list
+        valueEntropy = computeEntropy(valuesExpectedInfo, valuesProbability)
+        valueGain = allExpectedInfo - valueEntropy
+        valuesGain.append(valueGain)
+
+    # The index of the attribute with the max gain
+    index = valuesGain.index(max(valuesGain))
+
+    return [attributes[index], valuesGain[index], index]
 
 
-# Returns a list of the unique values of the given attribute in the given examples
-# TODO: implement
-def uniqueValues(attr, examples):
-    return []
+# Returns the expected info needed
+# count1 is usually the number of positive examples
+# count2 is usually num of negative examples
+def computeExpectedInfo(count1, count2):
+    count1 = float(count1)
+    count2 = float(count2)
+    total = count1 + count2
+    prob1 = count1/total
+    prob2 = count2/total
+
+    if prob1 > 0.0 and prob2 > 0.0:
+        return -prob1 * math.log(prob1, 2.0) - prob2 * math.log(prob2, 2.0)
+    elif prob1 > 0.0:
+        return -prob1 * math.log(prob1, 2.0)
+    elif prob2 > 0.0:
+        return -prob2 * math.log(prob2, 2.0)
+    else:
+        print 'YOU SHOULD PROBABLY NEVER SEE THIS.'
+        return 0
+
+
+# Compute entropy where p is a list of probabilities for each value and e is a
+# list of expected info for each value. p and e should be the same length.
+def computeEntropy(p, e):
+    entropy = 0.0
+    for i in range(len(p)):
+        entropy += p[i] * e[i]
+    return entropy
+
+
+# Returns a list of the unique values of the given attribute (given by index) in the given examples
+def uniqueValues(attrIndex, examples):
+    values = []
+    for e in examples:
+        if e[attrIndex] not in values:
+            values.append(e[attrIndex])
+    return values
 
 
 # Returns the most common value of the attribute in the examples
@@ -86,3 +204,24 @@ def getMostCommonValue(attr, examples, values):
 
     maxIndex = valueCounts.index(max(valueCounts))
     return values[maxIndex]
+
+
+# Returns the decision tree from a file containing training data
+# where the first line contains the attributes separated by commas
+# and each other line contains a data example
+def constructTreeFromFile(filepath):
+    f = open(filepath, 'r')
+    attrLine = f.readline()
+    attributes = [a.strip() for a in attrLine.split(',')]
+    examples = []
+    for line in f:
+        example = [item.strip() for item in line.split(',')]
+        examples.append(example)
+
+    # The last attribute is always the target attribute
+    return constructDecisionTree(examples, attributes[-1], attributes)
+
+
+path = raw_input('Please enter the path to a file containing training data:\n')
+tree = constructTreeFromFile(path)
+tree.printTree()
